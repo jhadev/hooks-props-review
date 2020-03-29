8d1b3af3a07df131a0add0fe5ae12c01fd51fcee
jhadev <joshappeldev@gmail.com>
2020-03-29 11:58:00 -0400

    create last name reset

--- src/components/InputFields.js
+++ src/components/InputFields.js
@@ -1,5 +1,12 @@
-import React, { useState } from 'react';
-import { Input, Heading, Button, Flex } from '@chakra-ui/core';
+import React, { useState, useEffect } from 'react';
+import {
+  Input,
+  InputGroup,
+  Heading,
+  Button,
+  Flex,
+  InputRightElement
+} from '@chakra-ui/core';
 
 const InputFields = props => {
   const [animationDirection, setAnimationDirection] = useState('in');
@@ -19,7 +26,7 @@ const InputFields = props => {
         animationDirection === 'in' ? 'scale-in-center' : 'scale-out-center'
       }>
       <Flex align="center">
-        <Heading textAlign="center" mr={4} my={[2, 2, 3]} as="h3" size="xl">
+        <Heading textAlign="center" mr={4} my={2} as="h3" size="xl">
           {props.header}
         </Heading>
         {/* if there is both a firstName and lastName prop and they are truthy (!== '') render a reset button */}
@@ -33,20 +40,31 @@ const InputFields = props => {
         {props.firstName} {props.lastName}
       </Heading>
       {/* input fields value are dependent on props passed in. along with the update function for each */}
-      <Input
-        my={3}
-        placeholder={`${props.header} first name`}
-        size="lg"
-        value={props.firstName}
-        onChange={e => props.setFirstName(e.target.value)}
-      />
-      <Input
-        my={3}
-        placeholder={`${props.header} last name`}
-        size="lg"
-        value={props.lastName}
-        onChange={e => props.setLastName(e.target.value)}
-      />
+      <InputGroup size="md">
+        <Input
+          my={3}
+          placeholder={`${props.header} first name`}
+          size="lg"
+          value={props.firstName}
+          onChange={e => props.setFirstName(e.target.value)}
+        />
+      </InputGroup>
+      <InputGroup size="md">
+        <Input
+          my={3}
+          placeholder={`${props.header} last name`}
+          size="lg"
+          value={props.lastName}
+          onChange={e => props.setLastName(e.target.value)}
+        />
+        {props.lastName && (
+          <InputRightElement mt={3} width="3rem">
+            <Button h="1.75rem" size="sm" onClick={() => props.setLastName('')}>
+              X
+            </Button>
+          </InputRightElement>
+        )}
+      </InputGroup>
     </div>
   );
 };
